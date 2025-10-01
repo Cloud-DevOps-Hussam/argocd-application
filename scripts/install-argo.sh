5e3bc46f78ac5892726aa764d40115576141fe3c
Hossam M.Shawky <52916569+hossamShawky@users.noreply.github.com>
2025-10-01 14:59:24 +0300

    Update install-argo.sh

--- scripts/install-argo.sh
+++ scripts/install-argo.sh
@@ -9,8 +9,13 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v3
 kubectl get pods -n argocd
 kubectl  -n argocd wait --for=condition=available --timeout=600s deployment/argocd-server
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo >> ./argo-admin-pass.txt
+
+
+kubectl patch deployment argocd-server -n argocd \
 --type='json' \
 -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--insecure"}]'
+
+
 kubectl  -n argocd wait --for=condition=available --timeout=600s deployment/argocd-server
 
 kubectl port-forward svc/argocd-server -n argocd 8080:443
